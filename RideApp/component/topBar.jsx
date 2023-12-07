67bb3c41471fe10efaa2e38d4e046381d3fe965b
Yashmika Anusara <yashmikaaunusara@gmail.com>
2023-12-07 13:05:17 +0530

    Update topBar.jsx

--- RideApp/component/topBar.jsx
+++ RideApp/component/topBar.jsx
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
 
 export default function TopBar() {
   return (
-    <View>
+    <View style={styles.display}>
       <Text>TopBar</Text>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  topBraContainer: {
+    display: "flex",
+  },
+});
